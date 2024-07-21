2792fdf4dffd3ca343bb9fc9330d41e56d627c02
KekJoy <valiullin.anton2003@yandex.ru>
2024-07-21 21:01:25 +0500

    fix: add router in main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from starlette.middleware.cors import CORSMiddleware
 
 from auth.service import auth_router
 from config import settings
+from grid_generator.services.grid import grid_router
 from tournaments.services.sport import sport_router
 from tournaments.services.tournament import tournament_router
 
@@ -24,3 +25,4 @@ async def root():
 app.include_router(auth_router, tags=['auth'])
 app.include_router(tournament_router, tags=['tournaments'])
 app.include_router(sport_router, tags=['sport'])
+app.include_router(grid_router, tags=['grids'])
