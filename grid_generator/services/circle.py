79853cd8b645d007dbf8257777a18733e3d9558e
TemaSaur <69428015+TemaSaur@users.noreply.github.com>
2024-07-26 02:50:20 +0500

    fix: circle queue order

--- grid_generator/services/circle.py
+++ grid_generator/services/circle.py
@@ -4,17 +4,46 @@ from grid_generator.models.schemas import PlayerResultSchema
 from grid_generator.repository import RoundRepository, MatchRepository
 
 
+async def get_circle_order(player_count: int) -> list[tuple[int, int]]:
+    order = [(0, 0)]
+
+    for group_size in range(2, player_count // 2 + 2):
+        group_pairs = 0
+
+        first = 0
+        while group_pairs < player_count:
+            if player_count & 1 == 0 and group_pairs == player_count // 2 and group_size == player_count // 2 + 1:
+                break
+            second = (first + group_size - 1) % player_count
+            while first in order[-1] or second in order[-1] or (first, second) in order:
+                first = (first + 1) % player_count
+                second = (second + 1) % player_count
+                if group_size == player_count // 2 or player_count <= 4:
+                    break
+            order.append(tuple(sorted((first, second))))
+            group_pairs += 1
+            first = (second + 1) % player_count
+    return order[1:]
+
+
+async def get_queue_number_dict(order: list[tuple[int, int]]) -> dict[tuple[int, int], int]:
+    return {v: k for k, v in enumerate(order)}
+
+
 async def create_circle(shuffled_players: list[uuid.UUID], grid_id: uuid.UUID) -> list:
     players_count = len(shuffled_players)
     round_id = await RoundRepository().add_round(round_number=1, grid_id=grid_id)
     current_match_number = 1
     matches = []
+    match_queue_order = await get_circle_order(players_count)
+    get_queue_number = await get_queue_number_dict(match_queue_order)
     for i in range(players_count - 1):
         for j in range(i + 1, players_count):
             match_players = [shuffled_players[i], shuffled_players[j]]
             _match = await MatchRepository().add_round_match(
                 round_id=round_id,
-                number=current_match_number,
+                grid_number=current_match_number,
+                queue_number=get_queue_number[(i, j)],
                 players=match_players)
             matches.append(_match)
             current_match_number += 1
